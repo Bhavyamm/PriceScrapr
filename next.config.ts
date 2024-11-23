bd59739d5ef255cca20e761f42ab0b68f221521a
Bhavyamm <bhavyamm01@gmail.com>
2024-11-23 22:00:29 +0530

    Added navbar

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // serverActions: true,
+    serverComponentsExternalPackages: ['mongoose']
+  },
+  images: {
+    domains: ['m.media-amazon.com']
+  }
 };
 
 export default nextConfig;
